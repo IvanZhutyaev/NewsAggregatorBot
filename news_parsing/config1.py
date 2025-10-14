1c13a279e2dc8045046ec5a4da0166a292f7796f
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-10-14 20:46:07 +0300

    complete

---
+++ news_parsing/config1.py
@@ -0,0 +1,8 @@
+BOT_TOKEN = "8446880916:AAEaK1EeD9yyFe6dr31ySfufNSMe0vWo1Vc"
+CHANNEL_ID = -1003125901221
+DEEPSEEK_KEY = "sk-0c19f7b4a67943fa9c008063910b9e97"
+ADMINS = [1621866383, 918713634, 535801213]
+# === Настройки для публикации на сайт ===
+SITE_LOGIN = "ajlin4277@gmail.com"
+SITE_PASSWORD = "Ab123456789"
+SITE_ADD_NEWS_URL = "https://api.demo.agrosearch.kz"  # URL формы добавления новости
