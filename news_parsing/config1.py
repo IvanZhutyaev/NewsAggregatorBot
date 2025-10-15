b95fe8cadbbf2077d4d8f658714716ad45ed5696
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-10-15 19:13:05 +0300

    swap configs

--- news_parsing/config1.py
+++ news_parsing/config1.py
@@ -1,8 +1,8 @@
-BOT_TOKEN = "8446880916:AAEaK1EeD9yyFe6dr31ySfufNSMe0vWo1Vc"
-CHANNEL_ID = -1003125901221
+BOT_TOKEN = "7860144131:AAEzwawng1BJjVJ-RtTwVcYBNzcPsxghF4g"
+CHANNEL_ID = -1002712994446  # или ID канала
 DEEPSEEK_KEY = "sk-0c19f7b4a67943fa9c008063910b9e97"
-ADMINS = [1621866383, 918713634, 535801213]
-# === Настройки для публикации на сайт ===
-SITE_LOGIN = "ajlin4277@gmail.com"
-SITE_PASSWORD = "Ab123456789"
-SITE_ADD_NEWS_URL = "https://api.demo.agrosearch.kz"  # URL формы добавления новости
+ADMINS = [5225476657]  # твой Telegram ID
+#  === Настройки для публикации на сайт ===
+SITE_LOGIN = "ajlin4277@gmail.com"                    # логин для админки сайта
+SITE_PASSWORD = "Ab123456789"           # пароль для админки сайта
+SITE_URL = "https://api.demo.agrosearch.kz"  # URL формы добавления новости
