f65756275bba70c4928b827fe07ca0d1f546478f
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-10-17 17:49:32 +0300

    complete

--- news_parsing/config1.py
+++
@@ -1,8 +0,0 @@
-BOT_TOKEN = ""
-CHANNEL_ID = ""  # или ID канала
-DEEPSEEK_KEY = ""
-ADMINS = []  # твой Telegram ID
-#  === Настройки для публикации на сайт ===
-SITE_LOGIN = ""  # логин для админки сайта
-SITE_PASSWORD = ""  # пароль для админки сайта
-SITE_URL = ""  # URL формы добавления новости
