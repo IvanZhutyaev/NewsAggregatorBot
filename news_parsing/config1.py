bf261b05e656687ae0e1759bbcec6ef511c57865
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-10-17 16:24:08 +0300

    complete

--- news_parsing/config1.py
+++
@@ -1,8 +0,0 @@
-BOT_TOKEN = "7860144131:AAEzwawng1BJjVJ-RtTwVcYBNzcPsxghF4g"
-CHANNEL_ID = -1002712994446  # или ID канала
-DEEPSEEK_KEY = "sk-0c19f7b4a67943fa9c008063910b9e97"
-ADMINS = [5225476657]  # твой Telegram ID
-#  === Настройки для публикации на сайт ===
-SITE_LOGIN = "ajlin4277@gmail.com"                    # логин для админки сайта
-SITE_PASSWORD = "Ab123456789"           # пароль для админки сайта
-SITE_URL = "https://api.demo.agrosearch.kz"  # URL формы добавления новости
